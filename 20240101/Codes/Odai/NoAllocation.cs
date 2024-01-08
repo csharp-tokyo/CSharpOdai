de87bda9f39a76de630da7f9b487bb0c570e7494
aetos <aetos382@users.noreply.github.com>
2024-01-09 00:20:53 +0900

    range

--- 20240101/Codes/Odai/NoAllocation.cs
+++ 20240101/Codes/Odai/NoAllocation.cs
@@ -14,9 +14,9 @@ public readonly struct NoAllocation :
 
         input[6..10].CopyTo(output);
         output[4] = '/';
-        input[0..2].CopyTo(output.Slice(5));
+        input[0..2].CopyTo(output[5..7]);
         output[7] = '/';
-        input[3..5].CopyTo(output.Slice(8));
+        input[3..5].CopyTo(output[8..10]);
     }
 
     public static readonly NoAllocation Instance = new();
