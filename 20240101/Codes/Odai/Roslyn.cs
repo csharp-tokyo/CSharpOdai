3340bd6b59d32d42b00ff0ff103a8a4ce3be3606
aetos <aetos382@users.noreply.github.com>
2024-01-09 00:46:38 +0900

    simplify

--- 20240101/Codes/Odai/Roslyn.cs
+++ 20240101/Codes/Odai/Roslyn.cs
@@ -18,6 +18,7 @@ public sealed class Roslyn :
         var input = SyntaxFactory.IdentifierName("input");
         var output = SyntaxFactory.IdentifierName("output");
         var culture = SyntaxFactory.IdentifierName("culture");
+        var cultureType = SyntaxFactory.ParseTypeName(nameof(CultureInfo));
 
         var compilationUnit = SyntaxFactory.CompilationUnit()
             .WithUsings(SyntaxFactory.List(
@@ -31,62 +32,60 @@ public sealed class Roslyn :
                     .WithModifiers(SyntaxFactory.TokenList(
                         SyntaxFactory.Token(SyntaxKind.PublicKeyword),
                         SyntaxFactory.Token(SyntaxKind.StaticKeyword)))
-                    .WithMembers(SyntaxFactory.List<MemberDeclarationSyntax>(
-                        [
-                            SyntaxFactory
-                                .MethodDeclaration(
-                                    SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.VoidKeyword)),
-                                    SyntaxFactory.Identifier("Invoke"))
-                                .WithModifiers(SyntaxFactory.TokenList(
-                                    SyntaxFactory.Token(SyntaxKind.PublicKeyword),
-                                    SyntaxFactory.Token(SyntaxKind.StaticKeyword)))
-                                .WithParameterList(SyntaxFactory.ParameterList(
-                                    SyntaxFactory.SeparatedList(
-                                        [
+                    .WithMembers(SyntaxFactory.SingletonList<MemberDeclarationSyntax>(
+                        SyntaxFactory
+                            .MethodDeclaration(
+                                SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.VoidKeyword)),
+                                SyntaxFactory.Identifier("Invoke"))
+                            .WithModifiers(SyntaxFactory.TokenList(
+                                SyntaxFactory.Token(SyntaxKind.PublicKeyword),
+                                SyntaxFactory.Token(SyntaxKind.StaticKeyword)))
+                            .WithParameterList(SyntaxFactory.ParameterList(
+                                SyntaxFactory.SeparatedList(
+                                    [
+                                        SyntaxFactory
+                                            .Parameter(input.Identifier)
+                                            .WithType(SyntaxFactory.ParseTypeName("ReadOnlySpan<char>")),
+                                        SyntaxFactory
+                                            .Parameter(output.Identifier)
+                                            .WithType(SyntaxFactory.ParseTypeName("Span<char>"))
+                                    ])))
+                            .WithBody(SyntaxFactory.Block(
+                                SyntaxFactory.LocalDeclarationStatement(
+                                    SyntaxFactory.VariableDeclaration(
+                                        cultureType,
+                                        SyntaxFactory.SingletonSeparatedList(
                                             SyntaxFactory
-                                                .Parameter(input.Identifier)
-                                                .WithType(SyntaxFactory.ParseTypeName("ReadOnlySpan<char>")),
-                                            SyntaxFactory
-                                                .Parameter(output.Identifier)
-                                                .WithType(SyntaxFactory.ParseTypeName("Span<char>"))
-                                        ])))
-                                .WithBody(SyntaxFactory.Block(
-                                    SyntaxFactory.LocalDeclarationStatement(
-                                        SyntaxFactory.VariableDeclaration(
-                                            SyntaxFactory.IdentifierName(SyntaxFactory.Identifier(default, SyntaxKind.VarKeyword, "var", "var", default)),
-                                            SyntaxFactory.SingletonSeparatedList(
-                                                SyntaxFactory
-                                                    .VariableDeclarator(culture.Identifier)
-                                                    .WithInitializer(
-                                                        SyntaxFactory.EqualsValueClause(
-                                                            SyntaxFactory.MemberAccessExpression(
-                                                                SyntaxKind.SimpleMemberAccessExpression,
-                                                                SyntaxFactory.ParseTypeName(nameof(CultureInfo)),
-                                                                SyntaxFactory.IdentifierName(nameof(CultureInfo.InvariantCulture)))))))),
-                                    SyntaxFactory.ExpressionStatement(
-                                        SyntaxFactory.InvocationExpression(
-                                            SyntaxFactory.MemberAccessExpression(
-                                                SyntaxKind.SimpleMemberAccessExpression,
-                                                SyntaxFactory.InvocationExpression(
-                                                    SyntaxFactory.MemberAccessExpression(
-                                                        SyntaxKind.SimpleMemberAccessExpression,
-                                                        SyntaxFactory.ParseName(nameof(DateTime)),
-                                                        SyntaxFactory.IdentifierName(nameof(DateTime.ParseExact))),
-                                                    SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(
-                                                        [
-                                                            SyntaxFactory.Argument(input),
-                                                            SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal("MM-dd-yyyy"))),
-                                                            SyntaxFactory.Argument(culture)
-                                                        ]))),
-                                                SyntaxFactory.IdentifierName(nameof(DateTime.TryFormat))),
-                                            SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(
-                                                [
-                                                    SyntaxFactory.Argument(output),
-                                                    SyntaxFactory.Argument(default, SyntaxFactory.Token(SyntaxKind.OutKeyword), SyntaxFactory.IdentifierName(SyntaxFactory.Identifier(default, SyntaxKind.UnderscoreToken, "_", "_", default))),
-                                                    SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal("yyyy/MM/dd"))),
-                                                    SyntaxFactory.Argument(culture)
-                                                ]))))))
-                        ]))));
+                                                .VariableDeclarator(culture.Identifier)
+                                                .WithInitializer(
+                                                    SyntaxFactory.EqualsValueClause(
+                                                        SyntaxFactory.MemberAccessExpression(
+                                                            SyntaxKind.SimpleMemberAccessExpression,
+                                                            cultureType,
+                                                            SyntaxFactory.IdentifierName(nameof(CultureInfo.InvariantCulture)))))))),
+                                SyntaxFactory.ExpressionStatement(
+                                    SyntaxFactory.InvocationExpression(
+                                        SyntaxFactory.MemberAccessExpression(
+                                            SyntaxKind.SimpleMemberAccessExpression,
+                                            SyntaxFactory.InvocationExpression(
+                                                SyntaxFactory.MemberAccessExpression(
+                                                    SyntaxKind.SimpleMemberAccessExpression,
+                                                    SyntaxFactory.ParseName(nameof(DateTime)),
+                                                    SyntaxFactory.IdentifierName(nameof(DateTime.ParseExact))),
+                                                SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(
+                                                    [
+                                                        SyntaxFactory.Argument(input),
+                                                        SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal("MM-dd-yyyy"))),
+                                                        SyntaxFactory.Argument(culture)
+                                                    ]))),
+                                            SyntaxFactory.IdentifierName(nameof(DateTime.TryFormat))),
+                                        SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(
+                                            [
+                                                SyntaxFactory.Argument(output),
+                                                SyntaxFactory.Argument(default, SyntaxFactory.Token(SyntaxKind.OutKeyword), SyntaxFactory.IdentifierName(SyntaxFactory.Identifier(default, SyntaxKind.UnderscoreToken, "_", "_", default))),
+                                                SyntaxFactory.Argument(SyntaxFactory.LiteralExpression(SyntaxKind.StringLiteralExpression, SyntaxFactory.Literal("yyyy/MM/dd"))),
+                                                SyntaxFactory.Argument(culture)
+                                            ]))))))))));
 
         var syntaxTree = CSharpSyntaxTree.Create(
             compilationUnit,
