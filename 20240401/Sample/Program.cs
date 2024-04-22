087d5b06ab7904e813df8ce00493f79525265ee8
m-ishizaki <love.ms.is12t@hotmail.co.jp>
2024-04-23 02:02:46 +0900

    Add Odai 202404

---
+++ 20240401/Sample/Program.cs
@@ -0,0 +1,3 @@
+﻿string[][] data = args?.FirstOrDefault()?.Split('\n').Select(x => x.Split(',')).ToArray() ?? new string[0][];
+data = int.TryParse(args?.Skip(1).FirstOrDefault(), out var index) ? data.OrderBy(x => x?.Skip(index).FirstOrDefault()).ToArray() : data;
+Console.WriteLine(string.Join("\n", data.Select(x => string.Join(",", x))));
